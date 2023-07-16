9217af834388f0fa923324e7eec6f86acd386cbd
frxcksz <rangga@upi.edu>
2023-07-16 23:55:34 +0700

    first commit

--- app/controllers/exampleController.js
+++ app/controllers/exampleController.js
@@ -1,118 +1,212 @@
 const db = require("../models");
 // const Model = db.Model;
 // const { Op } = require("sequelize");
+const WebSocket = require('ws');
+const axios = require('axios');
+const redis = require('redis');
+
+// Create a Redis client
+const redisClient = redis.createClient({
+  socket: {
+    host: '127.0.0.1',
+    port: 9999,
+  }
+})
+
+redisClient.connect();
+
+redisClient.on('connect', () => {
+  console.log('Connected');
+});
+
+redisClient.on('error', err => { 
+  console.log('Redis Server Error', err);
+  process.exit(1);
+});
+
+exports.exampleFunction = (req, res) => {
+  res.json({ message: "Hello" });
+};
 
 exports.refactoreMe1 = (req, res) => {
   // function ini sebenarnya adalah hasil survey dri beberapa pertnayaan, yang mana nilai dri jawaban tsb akan di store pada array seperti yang ada di dataset
-  db.sequelize.query(`select * from "surveys"`).then((data) => {
-    let index1 = [];
-    let index2 = [];
-    let index3 = [];
-    let index4 = [];
-    let index5 = [];
-    let index6 = [];
-    let index7 = [];
-    let index8 = [];
-    let index9 = [];
-    let index10 = [];
+  const data = db.sequelize.query(`select * from surveys`);
+  const indexes = Array.from(Array(10), () => []);
 
-    data.map((e) => {
-      let values1 = e.values[0];
-      let values2 = e.values[1];
-      let values3 = e.values[2];
-      let values4 = e.values[3];
-      let values5 = e.values[4];
-      let values6 = e.values[5];
-      let values7 = e.values[6];
-      let values8 = e.values[7];
-      let values9 = e.values[8];
-      let values10 = e.values[9];
+  data.map((e) => {
+    for (let i = 0; i < 10; i++) {
+      indexes[i].push(e.values[i]);
+    }
+  });
 
-      index1.push(values1);
-      index2.push(values2);
-      index3.push(values3);
-      index4.push(values4);
-      index5.push(values5);
-      index6.push(values6);
-      index7.push(values7);
-      index8.push(values8);
-      index9.push(values9);
-      index10.push(values10);
-    });
+  const totalIndexes = indexes.map((index) => {
+    return index.reduce((a, b) => a + b, 0) / 10;
+  });
 
-    let totalIndex1 = index1.reduce((a, b) => a + b, 0) / 10;
-    let totalIndex2 = index2.reduce((a, b) => a + b, 0) / 10;
-    let totalIndex3 = index3.reduce((a, b) => a + b, 0) / 10;
-    let totalIndex4 = index4.reduce((a, b) => a + b, 0) / 10;
-    let totalIndex5 = index5.reduce((a, b) => a + b, 0) / 10;
-    let totalIndex6 = index6.reduce((a, b) => a + b, 0) / 10;
-    let totalIndex7 = index7.reduce((a, b) => a + b, 0) / 10;
-    let totalIndex8 = index8.reduce((a, b) => a + b, 0) / 10;
-    let totalIndex9 = index9.reduce((a, b) => a + b, 0) / 10;
-    let totalIndex10 = index10.reduce((a, b) => a + b, 0) / 10;
-
-    let totalIndex = [
-      totalIndex1,
-      totalIndex2,
-      totalIndex3,
-      totalIndex4,
-      totalIndex5,
-      totalIndex6,
-      totalIndex7,
-      totalIndex8,
-      totalIndex9,
-      totalIndex10,
-    ];
-
-    res.status(200).send({
-      statusCode: 200,
-      success: true,
-      data: totalIndex,
-    });
+  res.status(200).send({
+    statusCode: 200,
+    success: true,
+    data: totalIndexes,
   });
 };
 
-exports.refactoreMe2 = (req, res) => {
+exports.refactoreMe2 = async (req, res) => {
   // function ini untuk menjalakan query sql insert dan mengupdate field "dosurvey" yang ada di table user menjadi true, jika melihat data yang di berikan, salah satu usernnya memiliki dosurvey dengan data false
-  Survey.create({
-    userId: req.body.userId,
-    values: req.body.values, // [] kirim array
-  })
-    .then((data) => {
-      User.update(
-        {
-          dosurvey: true,
-        },
-        {
-          where: { id: req.body.id },
-        }
-      )
-        .then(() => {
-          console.log("success");
-        })
-        .catch((err) => console.log(err));
-
-      res.status(201).send({
-        statusCode: 201,
-        message: "Survey sent successfully!",
-        success: true,
-        data,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send({
-        statusCode: 500,
-        message: "Cannot post survey.",
-        success: false,
-      });
+  try {
+    const survey = await Survey.create({
+      userId: req.body.userId,
+      values: req.body.values, // [] kirim array
     });
+
+    await User.update(
+      { dosurvey: true },
+      { where: { id: req.body.id } }
+    );
+
+    console.log("success");
+
+    res.status(201).send({
+      statusCode: 201,
+      message: "Survey successfully!",
+      success: true,
+      data: survey,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      statusCode: 500,
+      message: "Cannot post survey.",
+      success: false,
+    });
+  }
 };
 
 exports.callmeWebSocket = (req, res) => {
-  // do something
+  // WebSocket server
+  const wss = new WebSocket.Server({ noServer: true });
+
+  // fetch data from API
+  async function fetchData() {
+    try {
+      const response = await axios.get('https://livethreatmap.radware.com/api/map/attacks?limit=10');
+      const data = response.data;
+
+      // send the fetched data to all connected WebSocket clients
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(data));
+        }
+      });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  // fetch data every 3 minutes
+  function fetchPeriodically() {
+    fetchData(); // fetch immediately
+
+    // schedule the next data fetch after 3 minutes
+    setTimeout(fetchPeriodically, 3 * 60 * 1000);
+  }
+
+  // WebSocket connection handler
+  function handleWebSocketConnection(ws) {
+    console.log('WebSocket client connected');
+
+    // connected client
+    ws.send('Welcome to the WebSocket server!');
+
+    // client closing the connection
+    ws.on('close', () => {
+      console.log('WebSocket client disconnected');
+    });
+  }
+
+  // attach WSS to the HTTP server
+  const server = res.socket.server;
+  server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+      wss.emit('connection', ws, request);
+    });
+  });
+
+  // fetching data every 3 minutes
+  fetchPeriodically();
+
+  res.status(200).send('WebSocket server running!');
 };
 
-exports.getData = (req, res) => {
-  // do something
+exports.getData = async (req, res) => {
+  try {
+    // Check the data in Redis cache
+    redisClient.get("attackStatistics", async (err, cachedData) => {
+      if (cachedData) {
+        // Return the cached data
+        const data = JSON.parse(cachedData);
+        res.status(200).json({
+          success: true,
+          statusCode: 200,
+          data,
+        });
+      } else {
+        // Fetch from API and store in cache
+        const response = await axios.get("https://livethreatmap.radware.com/api/map/attacks?limit=10");
+        const data = response.data;
+
+        // Store the data into the Redis cache with expiration of 1 hour
+        redisClient.setex("attackStatistics", 3600, JSON.stringify(data));
+
+        // Store the data into the database
+        await db.sequelize.query(
+          `INSERT INTO attacks ("destinationCountry", "sourceCountry", "attackType")
+          VALUES ${data
+            .map(
+              (item) =>
+                `('${item.destinationCountry}', '${item.sourceCountry}', '${item.attackType}')`
+            )
+            .join(", ")}`
+        );
+
+        // Retrieve attack statistics from the database
+        const result = await db.sequelize.query(`
+          SELECT "destinationCountry", COUNT(DISTINCT "attackType") as "totalTypes"
+          FROM attacks
+          GROUP BY "destinationCountry"
+        `);
+
+        // Response
+        const label = result[0].map((item) => item.destinationCountry);
+        const total = result[0].map((item) => parseInt(item.totalTypes));
+
+        res.status(200).json({
+          success: true,
+          statusCode: 200,
+          data: {
+            label,
+            total,
+          },
+        });
+      }
+
+      // Close the Redis client
+      redisClient.quit();
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      error: "Internal Server Error",
+    });
+  }
+};
+
+exports.protectedFunction = (req, res) => {
+  // protected route, just for "admin" role
+  res.json({
+    success: true,
+    statusCode: 200,
+    message: "Hello Admin",
+  });
 };
--- app/middleware/exampleMiddleware.js
+++ app/middleware/exampleMiddleware.js
@@ -1,12 +1,53 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/auth');
+
 const db = require("../models");
 // const model = db.model;
 
-exampleMiddlewareFunction = (req, res, next) => {
-  // do something
+const authenticateJWT = (req, res, next) => {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      statusCode: 401,
+      error: "Unauthorized",
+    });
+  }
+
+  jwt.verify(token, config.secret, (err, user) => {
+    if (err) {
+      return res.status(403).json({
+        success: false,
+        statusCode: 403,
+        error: "Forbidden",
+      });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+const checkUserRole = (requiredRole) => {
+  return (req, res, next) => {
+    const user = req.user;
+
+    if (!user || !user.role || user.role !== requiredRole) {
+      return res.status(403).json({
+        success: false,
+        statusCode: 403,
+        error: "Forbidden",
+      });
+    }
+
+    next();
+  };
 };
 
 const verify = {
-  exampleMiddlewareFunction: exampleMiddlewareFunction,
+  authenticateJWT: authenticateJWT,
+  checkUserRole: checkUserRole
 };
 
 module.exports = verify;
